e8d7bcb4a2ab2586bc1ff6901c3b80f2a3198ed9
ArindamBosch <arindamdutta1970@gmail.com>
2025-08-14 09:07:51 +0530

    Create ColorCoderTest.java

---
+++ colorcoder/ColorCoderTest.java
@@ -0,0 +1,17 @@
+package colorcoder;
+
+public class ColorCoderTest {
+
+    public static void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
+        ColorPair colorPair = ColorCoder.getColorFromPairNumber(pairNumber);
+        System.out.println("Got pair " + colorPair);
+        assert colorPair.getMajor() == expectedMajor;
+        assert colorPair.getMinor() == expectedMinor;
+    }
+
+    public static void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
+        int pairNumber = ColorCoder.getPairNumberFromColor(major, minor);
+        System.out.println("Got pair number " + pairNumber);
+        assert pairNumber == expectedPairNumber;
+    }
+}
